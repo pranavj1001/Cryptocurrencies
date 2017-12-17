dd045b3988eaba3bceed0460e97bdd38b65661a4
pranavj1001 <pranavj1001@gmail.com>
2017-12-17 19:29:26 +0530

    add a new window to display particular coin info

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const electron = require('electron');
 const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
+let coinWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
+  coinWindow = new BrowserWindow({});
+  coinWindow.loadURL(`file://${__dirname}/coin.html`);
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('coin:select', (event, coinData) => {
-  console.log(coinData);
+  mainWindow.close();
+  coinWindow.webContents.send('coin:data', coinData);
 });
