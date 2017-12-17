2b4772d61c8611028a44ce436f9b408a9dd3e19e
pranavj1001 <pranavj1001@gmail.com>
2017-12-17 18:31:24 +0530

    send info from mainWindow to electron app

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
+
+ipcMain.on('coin:select', (event, coinData) => {
+  console.log(coinData);
+});
